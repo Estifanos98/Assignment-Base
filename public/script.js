1ce0c968ef502e577879790132fd8b915d09079d
Estifanos Kidane <ekidane@terpmail.umd.edu>
2021-03-05 22:46:05 -0500

    Updated script.js

--- public/script.js
+++ public/script.js
@@ -29,4 +29,8 @@ function displayMatches() {
     suggestions.innerHTML=delEmptyAddress; 
  }
 
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
  
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
