a3ebf00c98bf6cd4239e1713e6e2154c7dd40b9d
yyemane32 <yohanayemane57@gmail.com>
2021-03-05 22:38:31 -0500

    Added display finction

--- public/script.js
+++ public/script.js
@@ -9,3 +9,24 @@ function findMatches(wordToMatch, restaurants) {
        return establishments.name.match(regex) || establishments.category.match(regex)
    });
 }
+
+function displayMatches() {
+    const matchArray = findMatches(this.value, restaurants);
+    const html = matchArray.map(establishments => {
+        return `
+         <li>
+            <span class="name">${establishments.name}</span><br>
+            <span class="category">${establishments.category}</span><br>
+            <span class="address"><address>${establishments.address_line_1}, ${establishments.address_line_2}, ${establishments.city}, ${establishments.state}, ${establishments.zip}
+            </address></span>
+        </li>
+        `;
+    }).join('');
+    suggestions.innerHTML=html;
+   
+    const replaceRegex = new RegExp("------,",'gi');
+    delEmptyAddress = html.replace(replaceRegex," ");
+    suggestions.innerHTML=delEmptyAddress; 
+ }
+
+ 
