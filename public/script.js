8ac500029543e4938707b9467c0cd612fe9d36ad
Estifanos Kidane <ekidane@terpmail.umd.edu>
2021-03-05 22:26:55 -0500

    added fetch api

--- public/script.js
+++ public/script.js
@@ -1,2 +1,11 @@
 const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
 const restaurants = [];  
+
+fetch(endpoint).then(blob => blob.json()).then(data => restaurants.push(...data));
+ 
+function findMatches(wordToMatch, restaurants) {
+   return restaurants.filter(establishments => {
+       const regex = new RegExp(wordToMatch, 'gi');
+       return establishments.name.match(regex) || establishments.category.match(regex)
+   });
+}
